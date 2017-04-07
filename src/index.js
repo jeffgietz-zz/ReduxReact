e777f6fccc899cc585bf3e02edad453ef5cab721
Jeffrey Gietz <jeffgietz@gmail.com>
2017-04-07 16:06:36 +1000

    Search on input change

--- src/index.js
+++ src/index.js
@@ -16,8 +16,13 @@ class App extends Component {
       selectedVideo: null
     };
 
+    this.videoSearch('giantbomb');
+
+  }
+
+  videoSearch(term) {
     // Search for surfboards by default
-    YTSearch({key: API_KEY, term: 'giantbomb'}, (videos) => {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({
         videos:videos,
         selectedVideo: videos[0]
@@ -28,7 +33,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList
           onVideoSelect = { selectedVideo => this.setState({selectedVideo}) }
